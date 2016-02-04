67a795842223159b27079a32d67c6eb9a19d142b
ifotn <rich@infrontofthenet.com>
2016-02-04 11:53:13 -0500

    End of class commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,4 +4,15 @@
     // IIFE - Immediately Invoked Function Expression or self-executing function
     console.log('App started...');
     
+    // named function
+    function replaceFirstParagraph() {
+        var firstParagraph;
+    
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "My new paragraph Data";
+        firstParagraph.className = "jumbotron";
+    }
+
+    // invoke named function
+    replaceFirstParagraph();
 })();
